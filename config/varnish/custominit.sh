1fb3200a264c498ed9dfb9bfc170676bc3cf06e7
jeff <jeff@modulaweb.fr>
2017-06-09 17:18:10 +0200

    typo and comments

--- config/varnish/custominit.sh
+++ config/varnish/custominit.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+# creates the files so that they really exist
 touch /varnishout/varnishadm
 touch /varnishout/varnishstatv
 
+# add the cronjobs
 crontab /cronjobs
 
+# launch the cron deamon
 crond
 
+# exec the original init script for the varnish repo
 exec /init.sh
